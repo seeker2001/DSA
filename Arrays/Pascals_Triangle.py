12b0d71ea190dac6398d8e60e4f258fa6a480696
Anurag Thakur <thakuranurag0306@gmail.com>
2023-10-20 13:18:30 +0530

    reformatted the file

--- Arrays/Pascals_Triangle.py
+++ Arrays/Pascals_Triangle.py
@@ -3,7 +3,8 @@ Leetcode: 118 (Variation 3)
 
 Problem Statement: This problem has 3 variations. They are stated below:
 
-Variation 1: Given row number r and column number c. Print the element at position (r, c) in Pascal's triangle.
+Variation 1: Given row number r and column number c. Print the element at position (r, c) in
+Pascal's triangle.
 Variation 2: Given the row number n. Print the n-th row of Pascal's triangle.
 Variation 3: Given the number of rows n. Print the first n rows of Pascal's triangle.
 
